5bfbcbe58ae2760de34bfa35e58a15a46eafc2ae
vaishnavisoni2605 <100957436+vaishnavisoni2605@users.noreply.github.com>
2022-04-10 11:25:21 -0600

    Update styles.js

--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -5,7 +5,7 @@ const styles = StyleSheet.create({
   container: RecipeCard.container,
   photo: RecipeCard.photo,
   title: RecipeCard.title,
-  category: RecipeCard.category
+  
 });
 
 export default styles;
