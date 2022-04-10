21bb9ae1e0858cb9b01e5f1cd8d43f9d16ab33d7
vaishnavisoni2605 <100957436+vaishnavisoni2605@users.noreply.github.com>
2022-04-10 11:17:30 -0600

    Adding data

---
+++ src/data/ingredientsdata.js
@@ -0,0 +1,26 @@
+import { Text } from 'react-native';
+import React, { Component } from 'react';
+import { recipes, ingredients } from './dataarray';
+
+export function getIngredientName(ingredientID) {
+  let name;
+  ingredients.map(data => {
+    if (data.ingredientId == ingredientID) {
+      name = data.name;
+    }
+  });
+  return name;
+}
+
+export function getAllIngredients(idArray) {
+  const ingredientsArray = [];
+  idArray.map(index => {
+    ingredients.map(data => {
+      if (data.ingredientId == index[0]) {
+        ingredientsArray.push([data, index[1]]);
+      }
+    });
+  });
+  return ingredientsArray;
+}
+
